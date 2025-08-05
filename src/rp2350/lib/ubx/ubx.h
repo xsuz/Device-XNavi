ccafd14cb157d9107f7419354951fa511cd68927
Yuta Suzuki <suzuki7yuta@gmail.com>
2025-08-06 01:18:39 +0900

    gnss関連の処理で使っていない変数を消した

--- src/rp2350/lib/ubx/ubx.h
+++ src/rp2350/lib/ubx/ubx.h
@@ -17,10 +17,6 @@ namespace ubx
     public:
         void parse(uint8_t);
         void reset();
-        inline int64_t get_last_commit_tick() const
-        {
-            return last_commit_time;
-        }
 
         inline void get_nav_pvt(NAV_PVT *pvt) const{
             uint8_t *bytes=(uint8_t*)pvt;
@@ -35,15 +31,15 @@ namespace ubx
         uint8_t buf[1024];
         enum State
         {
-            SYNC_CHAR1,
-            SYNC_CHAR2,
-            CLASS,
-            ID,
-            LENGTH_LSB,
-            LENGTH_MSB,
-            PAYLOAD,
-            CK_A,
-            CK_B
+            SYNC_CHAR1,// Synchronization character (0xB5)
+            SYNC_CHAR2,// Synchronization characters (0xB5, 0x62)
+            CLASS,// Message class
+            ID,// Message ID
+            LENGTH_LSB,// Length of payload (LSB)
+            LENGTH_MSB,// Length of payload (MSB)
+            PAYLOAD,// Payload
+            CK_A,// Checksum A
+            CK_B// Checksum B
         };
         State state = State::SYNC_CHAR1;
         union
@@ -60,12 +56,10 @@ namespace ubx
         uint8_t checksum_a = 0;
         uint8_t checksum_b = 0;
         uint8_t payload[1024] = {0};
-        int64_t last_commit_time = -1;
         union
         {
             NAV_PVT nav_pvt;
             uint8_t bytes[sizeof(NAV_PVT)];
         } nav_pvt_data;
-        bool nav_pvt_valid = false;
     };
 } // namespace ubx
