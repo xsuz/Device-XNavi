df56693976e2d0d2650a3ee1390509136ea817eb
Yuta Suzuki <suzuki7yuta@gmail.com>
2025-09-01 21:59:48 +0900

    README修正

--- src/rp2350/include/readme.h
+++ src/rp2350/include/readme.h
@@ -19,17 +19,17 @@ Each packet in the log file has a GPS timestamp(int64) at the beginning, followe
 
 |offset   |content     |format |endian       |unit    |
 |:-------:|:----------:|:-----:|:-----------:|:------:|
-| [ 0- 3] |sensor type |uint32 |Big Endian   |      |
-| [ 4- 7] |timestamp   |uint32 |Big Endian   |msec    |
-| [ 8-11] |latitude    |int32  |Big Endian   |1e-7 deg|
-| [12-15] |longitude   |int32  |Big Endian   |1e-7 deg|
-| [16-19] |altitude    |int32  |Big Endian   |mm      |
-| [20-23] |velN        |int32  |Big Endian   |mm/s    |
-| [24-27] |velE        |int32  |Big Endian   |mm/s    |
-| [28-31] |velD        |int32  |Big Endian   |mm/s    |
-| [32-36] |hAcc        |int32  |Big Endian   |mm      |
-| [36-40] |vAcc        |int32  |Big Endian   |mm      |
-| [40-41] |pDOP        |uint16 |Big Endian   |1e-2    |
+| [ 0- 3] |sensor type |uint32 |Little Endian|      |
+| [ 4- 7] |timestamp   |uint32 |Little Endian|msec    |
+| [ 8-11] |latitude    |int32  |Little Endian|1e-7 deg|
+| [12-15] |longitude   |int32  |Little Endian|1e-7 deg|
+| [16-19] |altitude    |int32  |Little Endian|mm      |
+| [20-23] |velN        |int32  |Little Endian|mm/s    |
+| [24-27] |velE        |int32  |Little Endian|mm/s    |
+| [28-31] |velD        |int32  |Little Endian|mm/s    |
+| [32-36] |hAcc        |int32  |Little Endian|mm      |
+| [36-40] |vAcc        |int32  |Little Endian|mm      |
+| [40-41] |pDOP        |uint16 |Little Endian|1e-2    |
 | 42      |fix type    |uint8  |             |        |
 | 43      |flags       |uint8  |             |        |
 
@@ -37,22 +37,22 @@ Each packet in the log file has a GPS timestamp(int64) at the beginning, followe
 
 |offset   |content     |format |endian       |unit  |
 |:-------:|:----------:|:-----:|:-----------:|:----:|
-| [ 0- 3] |sensor type |uint32 |Big Endian   |      |
-| [ 4- 7] |timestamp   |uint32 |Big Endian   |msec  |
-| [ 8-11] |a_x         |float32|Big Endian   |m/s^2 |
-| [12-15] |a_y         |float32|Big Endian   |m/s^2 |
-| [16-19] |a_z         |float32|Big Endian   |m/s^2 |
-| [20-23] |w_x         |float32|Big Endian   |rad/s |
-| [24-27] |w_y         |float32|Big Endian   |rad/s |
-| [28-31] |w_z         |float32|Big Endian   |rad/s |
+| [ 0- 3] |sensor type |uint32 |Little Endian|      |
+| [ 4- 7] |timestamp   |uint32 |Little Endian|msec  |
+| [ 8-11] |a_x         |float32|Little Endian|m/s^2 |
+| [12-15] |a_y         |float32|Little Endian|m/s^2 |
+| [16-19] |a_z         |float32|Little Endian|m/s^2 |
+| [20-23] |w_x         |float32|Little Endian|rad/s |
+| [24-27] |w_y         |float32|Little Endian|rad/s |
+| [28-31] |w_z         |float32|Little Endian|rad/s |
 
 ### 2.3. CAN bus
 
 |offset   |content     |format |endian       |unit  |
 |:-------:|:----------:|:-----:|:-----------:|:----:|
-| [ 0- 3] |ID          |uint32 |Big Endian   |      |
-| [ 4- 7] |timestamp   |uint32 |Big Endian   |msec  |
-| [ 8-11] |length      |uint32 |Big Endian   |      |
+| [ 0- 3] |ID          |uint32 |Little Endian|      |
+| [ 4- 7] |timestamp   |uint32 |Little Endian|msec  |
+| [ 8-11] |length      |uint32 |Little Endian|      |
 | [12-16] |payload     |uint8  |             |      |
 
 
